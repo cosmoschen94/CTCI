89e0ecfd0db1d69a7011e6af80c22f5dc14b58e2
cosmoschen94 <applepiefish@g.ucla.edu>
2015-10-16 18:26:47 -0700

    Add 1.2 first test

---
+++ reverse/reverse/main.cpp
@@ -0,0 +1,65 @@
+//
+//  main.cpp
+//  reverse
+//
+//  Created by Siyu Chen on 10/16/15.
+//  Copyright © 2015 Siyu Chen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+void reverse(char * str);
+
+void reverse(char * str)
+{
+    char *p = str;
+    unsigned long length = strlen(str);
+    char *t = p + (length - 1);
+    
+    //Base case
+    if (length == 1 || length == 0){return;}
+    
+    // even case
+    if (length % 2 == 0 && length != 2)
+    {
+        char * f = p + (length/2 + 1);
+        while (p != f)
+        {
+            char temp = *p;
+            *p = *t;
+            *t = temp;
+            p++;
+            t--;
+        }
+        return;
+    }
+    
+    // odd case
+    else
+    {
+        char * f = p + (length - 1)/2;
+        while (p != f)
+        {
+            char temp = *p;
+            *p = *t;
+            *t = temp;
+            p++;
+            t--;
+        }
+        return;
+    }
+    
+    
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    char * str = "hello";
+    reverse(str);
+    cout << str;
+    
+    
+}
