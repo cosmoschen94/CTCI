2aea1f22c2ca62c47c74ba8b8458ade5c1e002ea
cosmoschen94 <applepiefish@g.ucla.edu>
2015-10-17 11:30:46 -0700

    Fix bugs

--- permutation/permutation/main.cpp
+++ permutation/permutation/main.cpp
@@ -22,6 +22,45 @@ bool perm(string a, string b)
     if (a.length() != b.length()){return false;}
     if (a == b){return true;}
     
+    // allocate dynamic 2D array
+    int ** p;
+    p = new int*[256];
+    for (int i=0; i<256; ++i)
+    {
+        p[i] = new int[2];
+    }
+    
+    // count char for each string
+    for(int i = 0; i < a.length(); ++i)
+    {
+        int va = a[i];
+        int vb = b[i];
+        cout << va << "\t" << vb << "\n";
+        p[va][0] += 1;
+        p[vb][1] += 1;
+    }
+    
+    
+    cout << "Seperation======================" << "\n";
+    
+    // compare count
+    for (int i = 0; i<256; ++i)
+    {
+        cout << p[i][0] << "\t" << p[i][1];
+        cout << "\n";
+        if(p[i][0]!=p[i][1]){return false;}
+    }
+    
+    // deallocation
+    for (int i=0; i < 256; ++i)
+    {
+        delete [] p[i];
+    }
+    delete [] p;
+    
+    return true;
+    
+    /*
     int * hash;
     hash = new int[256]{0};
     //hash[256]={false};
@@ -60,16 +99,13 @@ bool perm(string a, string b)
             return false;
         }
     }
-        
-    
-    delete [] hash;
-    return true;
+        */
 }
 
 int main(int argc, const char * argv[]) {
     
     string a = "abcdad";
-    string b = "bddaaa";
+    string b = "bddaada";
     bool test = perm(a,b);
     cout << test << "\n";
     
